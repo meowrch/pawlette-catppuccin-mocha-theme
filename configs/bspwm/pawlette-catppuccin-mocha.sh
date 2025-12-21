4a18872c3abf91a4a20381eaf0ee41a04de37180
DIMFLIX <dimflix.official@gmail.com>
2025-12-21 15:24:04 +0300

    Added automatic application of gtk themes for bspwm and hyprland at startup

--- configs/bspwm/pawlette-catppuccin-mocha.sh
+++ configs/bspwm/pawlette-catppuccin-mocha.sh
@@ -12,6 +12,9 @@ bspc config window_gap 10
 # Set color scheme
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
+# Set gtk theme
+gsettings set org.gnome.desktop.interface gtk-theme 'pawlette-catppuccin-mocha'
+
 # Set icon theme
 gsettings set org.gnome.desktop.interface icon-theme 'pawlette-catppuccin-mocha'
 
